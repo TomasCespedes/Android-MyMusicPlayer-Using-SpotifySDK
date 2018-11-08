566f3534d085b3f15c82046724b245f342f04955
tomas123213 <tcesp15@stlawu.edu>
2018-11-07 21:39:21 -0500

    Playing activity button connection

--- app/src/main/java/edu/stlawu/finalproject/PlayingActivity.java
+++ app/src/main/java/edu/stlawu/finalproject/PlayingActivity.java
@@ -1,13 +1,86 @@
 package edu.stlawu.finalproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
 
 public class PlayingActivity extends AppCompatActivity {
+
+    // TextViews
+    private TextView currentsong;
+
+    // Buttons
+    private ImageButton playpausebutton;
+    private Button homebutton, searchbutton, librarybutton, playingbutton;
+
+    // Tracker for song playing status (play or pause)
+    private String currenttracker = "play";
+
+    // ImageViews
+    private ImageView song_iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playing);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Find views
+        currentsong = findViewById(R.id.current_song);
+        playpausebutton = findViewById(R.id.current_button);
+        song_iv = findViewById(R.id.song_iv);
+
+        // Find all the buttons for the bottom menu
+        homebutton = findViewById(R.id.homebtn);
+        searchbutton = findViewById(R.id.searchbtn);
+        librarybutton = findViewById(R.id.librarybtn);
+        playingbutton = findViewById(R.id.playingbtn);
+
+        // Home button to create a new activity
+        homebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PlayingActivity.this, MainActivity.class));
+            }
+        });
+
+        // Search button to create a new activity
+        searchbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PlayingActivity.this, SearchActivity.class));
+            }
+        });
+
+        // Libray button to create a new activity
+        librarybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PlayingActivity.this, LibraryActivity.class));
+            }
+        });
+
+        // Playing Button to create a new activity
+        playingbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PlayingActivity.this, PlayingActivity.class));
+            }
+        });
     }
 }
