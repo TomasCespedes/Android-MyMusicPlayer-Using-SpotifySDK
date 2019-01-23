1d76aa936b7369a570d73da95335b8c9418154f6
tomas123213 <tcesp15@stlawu.edu>
2019-01-23 16:23:25 -0500

    Refactor and Cleanup into Classes

--- app/src/main/java/edu/stlawu/finalproject/MainActivity.java
+++ app/src/main/java/edu/stlawu/finalproject/MainActivity.java
@@ -14,7 +14,6 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,16 +22,11 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import java.sql.Array;
-import java.util.List;
-
 import kaaes.spotify.webapi.android.SpotifyApi;
 import kaaes.spotify.webapi.android.SpotifyCallback;
 import kaaes.spotify.webapi.android.SpotifyError;
 import kaaes.spotify.webapi.android.SpotifyService;
 import kaaes.spotify.webapi.android.models.Pager;
-import kaaes.spotify.webapi.android.models.Playlist;
 import kaaes.spotify.webapi.android.models.PlaylistSimple;
 import retrofit.client.Response;
 
@@ -52,7 +46,6 @@ public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_CODE = 1337;
 
     // App remote for Spotify to control song playback
-    public SpotifyAppRemote mSpotifyAppRemote;
     private Track track;
     private String accessToken;
 
@@ -86,14 +79,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
-        // Connect to the remote
-        connectToRemote();
+        startService(new Intent(this, edu.stlawu.finalproject.RemoteService.class));
 
         // Call the method to get the token
         getToken();
-
-
-
     }
 
     /**
@@ -189,123 +178,81 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-
-    /**
-     * Connect to the remote so user can
-     * interact with songs
-     */
-    private void connectToRemote() {
-        /**
-         * Set the connection parameters
-         */
-        ConnectionParams connectionParams =
-                new ConnectionParams.Builder(CLIENT_ID)
-                        .setRedirectUri(REDIRECT_URI)
-                        .showAuthView(true)
-                        .build();
-
-        /**
-         * Connect app to remote
-         */
-        SpotifyAppRemote.connect(this, connectionParams,
-                new Connector.ConnectionListener() {
-
-                    @Override
-                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
-                        mSpotifyAppRemote = spotifyAppRemote;
-                        Log.d("MainActivity", "Connected! Yay!");
-
-                        // Now you can start interacting with App Remote
-                        connected();
-                    }
-
-                    @Override
-                    public void onFailure(Throwable throwable) {
-                        Log.e("MainActivity", throwable.getMessage(), throwable);
-
-                        // Something went wrong when attempting to connect! Handle errors here
-                    }
-                });
-
-    }
-
-
-    /**
-     * Our app is connected to spotify and streaming
-     * can now happen.
-     */
-    private void connected() {
-        // Play a playlist
-        mSpotifyAppRemote.getPlayerApi().play("spotify:track:0VgkVdmE4gld66l8iyGjgx");
-
-
-        // Call the subscribe to Playerstate Method
-        subscribetoPlayerState();
-
-        // Button for playing/pausing the current song
-        playpausebutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                // If song is currently playing, pause it
-                if (currenttracker == "play") {
-                    playpausebutton.setImageResource(R.drawable.playbutton);
-                    currenttracker = "pause";
-                    mSpotifyAppRemote.getPlayerApi().pause();
-                }
-                // If song is currently paused, play it
-                else if (currenttracker == "pause") {
-                    playpausebutton.setImageResource(R.drawable.pausebutton);
-                    currenttracker = "play";
-                    mSpotifyAppRemote.getPlayerApi().resume();
-
-                }
-            }
-        });
-    }
+//    /**
+//     * Our app is connected to spotify and streaming
+//     * can now happen.
+//     */
+//    private void connected() {
+//        // Play a playlist
+//        mSpotifyAppRemote.getPlayerApi().play("spotify:track:0VgkVdmE4gld66l8iyGjgx");
+//
+//
+//        // Call the subscribe to Playerstate Method
+//        subscribetoPlayerState();
+//
+//        // Button for playing/pausing the current song
+//        playpausebutton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                // If song is currently playing, pause it
+//                if (currenttracker == "play") {
+//                    playpausebutton.setImageResource(R.drawable.playbutton);
+//                    currenttracker = "pause";
+//                    mSpotifyAppRemote.getPlayerApi().pause();
+//                }
+//                // If song is currently paused, play it
+//                else if (currenttracker == "pause") {
+//                    playpausebutton.setImageResource(R.drawable.pausebutton);
+//                    currenttracker = "play";
+//                    mSpotifyAppRemote.getPlayerApi().resume();
+//
+//                }
+//            }
+//        });
+//    }
 
 
     /**
      *Subscribe to PlayerState and get current playing information
      * Uses the Spotify Remote
      */
-    private void subscribetoPlayerState() {
-
-        // Subscribe to PlayerState
-
-        mSpotifyAppRemote.getPlayerApi()
-                .subscribeToPlayerState()
-                .setEventCallback(new Subscription.EventCallback<PlayerState>() {
-
-
-                    // If a song is playing get the track name and artist name
-                    public void onEvent(PlayerState playerState) {
-                        track = playerState.track;
-
-                        Log.i("PLAYERSTATE", playerState.track.toString());
-                        if (track != null) {
-                            /**
-                             * Set data to views (track name by track artist)
-                             */
-                            currentsong.setText((track.name + " by " + track.artist.name));
-                            /**
-                             * Get Image for the track
-                             */
-
-
-                            mSpotifyAppRemote.getImagesApi().getImage(track.imageUri)
-                                    .setResultCallback(new CallResult.ResultCallback<Bitmap>() {
-                                        @Override
-                                        public void onResult(Bitmap bitmap) {
-                                            Drawable d = new BitmapDrawable(getResources(), bitmap);
-                                            song_iv.setImageDrawable(d);
-                                        }
-                                    });
-                        }
-                    }
-                });
-    }
+//    private void subscribetoPlayerState() {
+//
+//        // Subscribe to PlayerState
+//
+//        mSpotifyAppRemote.getPlayerApi()
+//                .subscribeToPlayerState()
+//                .setEventCallback(new Subscription.EventCallback<PlayerState>() {
+//
+//
+//                    // If a song is playing get the track name and artist name
+//                    public void onEvent(PlayerState playerState) {
+//                        track = playerState.track;
+//
+//                        Log.i("PLAYERSTATE", playerState.track.toString());
+//                        if (track != null) {
+//                            /**
+//                             * Set data to views (track name by track artist)
+//                             */
+//                            currentsong.setText((track.name + " by " + track.artist.name));
+//                            /**
+//                             * Get Image for the track
+//                             */
+//
+//
+//                            mSpotifyAppRemote.getImagesApi().getImage(track.imageUri)
+//                                    .setResultCallback(new CallResult.ResultCallback<Bitmap>() {
+//                                        @Override
+//                                        public void onResult(Bitmap bitmap) {
+//                                            Drawable d = new BitmapDrawable(getResources(), bitmap);
+//                                            song_iv.setImageDrawable(d);
+//                                        }
+//                                    });
+//                        }
+//                    }
+//                });
+//    }
 
     /**
      * When app stops, disconnect app from Spotify
@@ -315,7 +262,7 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
 
         // Disconnect from app
-        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+        //SpotifyAppRemote.disconnect(mSpotifyAppRemote);
     }
 
     /**
---
+++ app/src/main/java/edu/stlawu/finalproject/RemoteService.java
@@ -0,0 +1,111 @@
+package edu.stlawu.finalproject;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+public class RemoteService extends Service {
+    // Client ID and Redirect URI gotten from Spotify
+    private static final String CLIENT_ID = "377538ebcf9e4cdb9c4b5373e62a53a3";
+    private static final String REDIRECT_URI = "FinalProjectCS450://callback";
+
+    // App remote for Spotify to control song playback
+    public SpotifyAppRemote mSpotifyAppRemote;
+
+    public RemoteService() {
+
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        connectToRemote();
+
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    /**
+     * Connect to the remote so user can
+     * interact with songs
+     */
+    private void connectToRemote() {
+        /**
+         * Set the connection parameters
+         */
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .showAuthView(true)
+                        .build();
+
+        /**
+         * Connect app to remote
+         */
+        SpotifyAppRemote.connect(this, connectionParams,
+                new Connector.ConnectionListener() {
+
+                    @Override
+                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                        mSpotifyAppRemote = spotifyAppRemote;
+                        Log.d("MainActivity", "Connected! Yay!");
+
+                        // Now you can start interacting with App Remote
+                        connected();
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.e("MainActivity", throwable.getMessage(), throwable);
+
+                        // Something went wrong when attempting to connect! Handle errors here
+                    }
+                });
+
+    }
+
+
+    /**
+     * Our app is connected to spotify and streaming
+     * can now happen.
+     */
+    private void connected() {
+        // Play a playlist
+        mSpotifyAppRemote.getPlayerApi().play("spotify:track:5274I4mUMnYczyeXkGDWZN");
+
+        Log.e("Hello!", "connected");
+
+//        // Button for playing/pausing the current song
+//        playpausebutton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                // If song is currently playing, pause it
+//                if (currenttracker == "play") {
+//                    playpausebutton.setImageResource(R.drawable.playbutton);
+//                    currenttracker = "pause";
+//                    mSpotifyAppRemote.getPlayerApi().pause();
+//                }
+//                // If song is currently paused, play it
+//                else if (currenttracker == "pause") {
+//                    playpausebutton.setImageResource(R.drawable.pausebutton);
+//                    currenttracker = "play";
+//                    mSpotifyAppRemote.getPlayerApi().resume();
+//
+//                }
+//            }
+//        });
+    }
+
+
+}
